de137f263131da3602fc3ce63233c917de198e29
shradha001 <shradhamallik.dev@gmail.com>
2019-01-19 18:26:26 +0530

    Modify configurations

---
+++ config/respCodeAndMsg.js
@@ -0,0 +1,16 @@
+"use strict";
+
+module.exports = {
+  STATUS_CODE: {
+    OK: 200,
+    CREATED: 201,
+    INTERNAL_SERVER_ERROR: 500,
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    UNAUTHORIZED: 401
+  },
+  ERR_MSG: {
+    BAD_REQUEST: "Bad Request",
+    NOT_FOUND: "Not Found"
+  }
+};
