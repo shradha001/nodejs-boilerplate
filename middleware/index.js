b8db905392d9adeeb37118e9f97d45ae6e36cc96
shradha001 <shradhamallik.dev@gmail.com>
2019-01-19 23:00:17 +0530

    Split middleware into separate files

--- middleware/index.js
+++ middleware/index.js
@@ -1,29 +1,6 @@
 "use strict";
 
-const { isCelebrate } = require("celebrate");
-const logger = require("../log");
-
-function errorHandler(err, req, res, next) {
-  if (err) {
-    if (isCelebrate(err)) {
-      const readableMessage = err.details
-        .map(d => d.message.replace(/["]/gi, ""))
-        .join(", ");
-      const e = { message: readableMessage, data: {} };
-      res.status(400).json(e);
-    } else {
-      res.status(400).json({ message: "Bad Request", data: {} });
-    }
-  } else next();
-}
-
-class LoggerStream {
-  write(message) {
-    logger.info(message.substring(0, message.lastIndexOf("\n")));
-  }
-}
-
 module.exports = {
-  errorHandler,
-  LoggerStream
+  errorHandler: require("./errorHandler"),
+  LoggerStream: require("./loggerStream")
 };
